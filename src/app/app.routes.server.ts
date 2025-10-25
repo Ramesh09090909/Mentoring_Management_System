1cf897282342f23c8319afeabc163272a87b53a4
Ramesh Pradhan <rameshpradhan4651@gmail.com>
2025-10-25 18:24:23 +0530

    Refactor routes to use ServerRoute for SSR

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,18 +1,8 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MentorDashboardComponent } from './mentor-dashboard/mentor-dashboard.component';
-import { StudentPanelComponent } from './student-panel/student-panel.component';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'mentor/dashboard', component: MentorDashboardComponent },
-  { path: 'mentor/student-panel', component: StudentPanelComponent },
-  
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
 ];
