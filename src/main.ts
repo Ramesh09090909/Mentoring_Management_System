d3a6eeaeca12f9cd0d595c346928b4bef1637a60
Ramesh Pradhan <rameshpradhan4651@gmail.com>
2025-11-03 23:12:20 +0530

    Refactor main.ts to use new app structure

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component'; // ✅ Correct
-import { importProvidersFrom } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app/app.routes'; // ✅ Correct path
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';  
 
 
-bootstrapApplication(AppComponent, {
+
+bootstrapApplication(App, {
   providers: [
-    importProvidersFrom(ReactiveFormsModule),
-    provideRouter(appRoutes)
+    provideRouter(routes),
+    provideHttpClient() 
   ]
 });
