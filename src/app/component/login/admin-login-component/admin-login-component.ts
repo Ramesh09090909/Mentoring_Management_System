3bfbde23b271ea416b5213f952f6a7bf3a218195
Ramesh Pradhan <rameshpradhan4651@gmail.com>
2025-11-03 23:32:18 +0530

    Add files via upload

---
+++ src/app/component/login/admin-login-component/admin-login-component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Admin } from '../../../services/admin'; 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+
+@Component({
+  selector: 'app-admin-login-component',
+  imports: [CommonModule, FormsModule,RouterLink],
+  templateUrl: './admin-login-component.html',
+  styleUrls: ['./admin-login-component.css']
+})
+export class AdminLoginComponent {
+  email = '';
+  password = '';
+  message = '';
+
+  constructor(private adminService: Admin, private router: Router,private authService: AuthService   ) {} // ✅ Use AdminService
+
+  login() {
+    if (!this.email || !this.password) {
+      this.message = 'Please enter email and password';
+      return;
+    }
+
+    this.adminService.login(this.email, this.password).subscribe({
+      next: (response: string) => { // ✅ Add type
+        this.message = response;
+        if (response === 'Login successful') {
+          this.router.navigate(['/admin/dashboard']);
+        }
+      },
+      error: (error: any) => { // ✅ Add type
+        this.message = 'Invalid email or password';
+      }
+    });
+    this.authService.setUserType('admin');
+    this.router.navigate(['/admin/dashboard']);
+  }
+  goBackToLanding() {
+    this.router.navigate(['/']);
+  }
+}
